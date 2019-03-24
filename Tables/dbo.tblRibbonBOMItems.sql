001bcca2362a9ac5de79a4bc86bb7a2af11d8205
magicsly <bryan.eddy5@gmail.com>
2019-03-24 14:59:54 -0400

    Added permissions for scheduling users to write to table.

--- Tables/dbo.tblRibbonBOMItems.sql
+++ Tables/dbo.tblRibbonBOMItems.sql
@@ -5,3 +5,11 @@ CREATE TABLE [dbo].[tblRibbonBOMItems]
 GO
 ALTER TABLE [dbo].[tblRibbonBOMItems] ADD CONSTRAINT [PK_tblRibbonBOMItems] PRIMARY KEY CLUSTERED  ([AssemblyItemNumber]) ON [PRIMARY]
 GO
+GRANT DELETE ON  [dbo].[tblRibbonBOMItems] TO [NAA\SPB Premise SQL RO]
+GO
+GRANT INSERT ON  [dbo].[tblRibbonBOMItems] TO [NAA\SPB Premise SQL RO]
+GO
+GRANT SELECT ON  [dbo].[tblRibbonBOMItems] TO [NAA\SPB Premise SQL RO]
+GO
+GRANT UPDATE ON  [dbo].[tblRibbonBOMItems] TO [NAA\SPB Premise SQL RO]
+GO
